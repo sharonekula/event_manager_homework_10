0799dfedd0d5c01d9a44d876618298622aa9c3bc
sharonekula <se376@njit.edu>
2024-12-06 22:04:45 -0500

    Added email in Login Request to fix #5

--- tests/conftest.py
+++ tests/conftest.py
@@ -260,4 +260,4 @@ def user_response_data():
 
 @pytest.fixture
 def login_request_data():
-    return {"username": "john_doe_123", "password": "SecurePassword123!"}
+    return {"username": "john_doe_123","email":"john.doe.new@example.com", "password": "SecurePassword123!"}
